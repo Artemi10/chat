36730e0ff377213d4f87f64efa9d62aaaa3f13e5
lyaha <lyah.artem10@mailru>
2021-10-31 18:33:06 +0300

    initial commit

---
+++ chat-account/src/main/java/com/devanmejia/chataccount/ChatAccountApplication.java
@@ -0,0 +1,13 @@
+package com.devanmejia.chataccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAccountApplication.class, args);
+    }
+
+}
